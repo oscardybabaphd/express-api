ba894da4ec384bcc9618993af586974da711ac22
Oscar Itaba <OItaba@custodian.com>
2019-03-30 22:23:48 +0100

    database sql file added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 let pagination = require('./pagination');
+let bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
 
@@ -22,9 +23,11 @@ db.connect((err) => {
 app.use(cors({
     'Access-Control-Allow-Origin': '*'
 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/getall/:_page/:_per_page", (req, res) => {
-    let sql = 'SELECT * FROM sampletable';
+    let sql = 'SELECT * FROM sampletable order by id desc';
     db.query(sql, (err, result) => {
         if (err) {
             throw err;
@@ -39,7 +42,7 @@ app.get("/getall/:_page/:_per_page", (req, res) => {
 });
 
 app.get("/search/:start_date/:end_date/:_page/:_per_page", (req, res) => {
-    let sql = `SELECT * FROM sampletable where start_date >= replace('${req.params.start_date}','-','/') and end_date <= replace('${req.params.end_date}','-','/')`;
+    let sql = `SELECT * FROM sampletable where start_date >= '${req.params.start_date}' and end_date <= '${req.params.end_date}' order by id desc`;
     db.query(sql, (err, result) => {
         if (err) {
             throw err;
@@ -52,6 +55,44 @@ app.get("/search/:start_date/:end_date/:_page/:_per_page", (req, res) => {
         res.send(pages);
     });
 });
+
+app.post("/update", (req, res) => {
+    let body = req.body;
+    let sql = `UPDATE sampletable set city = '${body.city}',
+    start_date = '${body.start_date}',end_date = '${body.end_date}',
+    price = ${body.price}, status = '${body.status}', color = '${body.color}' where id = ${body.id}`;
+    console.log(sql);
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ mesage: 'success', status: "00" });
+    });
+});
+app.post("/add", (req, res) => {
+    let body = req.body;
+    let sql = 'INSERT INTO sampletable SET ?';
+    db.query(sql, body, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ mesage: 'success', status: "00", id: result.insertId });
+    })
+});
+
+app.get("/delete/:id", (req, res) => {
+    let sql = `DELETE FROM sampletable WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send({ mesage: 'success', status: "00" });
+    })
+});
+
 app.listen(3000, () => {
     console.log("server started on port 3000");
 });
